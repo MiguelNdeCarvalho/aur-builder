22f7f32a7029650257fc5d458e1d4b2a3e5a8522
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 01:47:29 +0100

    webui: init.py: Add start of SQLAlchemy and create db file

--- webui/ui/__init__.py
+++ webui/ui/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_toastr import Toastr
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 toastr = Toastr()
 
@@ -7,6 +12,9 @@ toastr = Toastr()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'qwgjhehnwgnwjgwjgpqwdqw'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    toastr.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -14,6 +22,14 @@ def create_app():
     app.register_blueprint(views)
     app.register_blueprint(auth)
 
-    toastr.init_app(app)
+    # flake8: noqa: F401
+    from .models import User
+
+    create_database(app)
 
     return app
+
+
+def create_database(app):
+    if not path.exists("ui/" + DB_NAME):
+        db.create_all(app=app)
