039385757ff3af0471037c98f616cbc16b428400
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 01:15:35 +0100

    webui: main.py: Add flask_toastr

--- webui/main.py
+++ webui/main.py
@@ -1,7 +1,9 @@
 from ui import create_app
-
+from flask_toastr import Toastr
 
 app = create_app()
 
+toastr = Toastr(app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
