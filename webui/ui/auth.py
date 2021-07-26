b55d709791e64ac5b569853cdbe7495d74015a60
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 01:15:54 +0100

    webui: auth.py: Add some checks for logins and register page

--- webui/ui/auth.py
+++ webui/ui/auth.py
@@ -1,10 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request, flash, redirect, url_for
 from flask.templating import render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        flash("Wrong Email or Password!", category='error')
+    
     return render_template('login.html')
 
 
@@ -13,6 +19,33 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>Sign Up</p>"
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # user = User.query.filter_by(email=email).first()
+        # if user:
+        #     flash('Email already exists.', category='error')
+        if len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 6:
+            flash('Password must be at least 6 characters.', category='error')
+        # else:
+        #     new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+        #         password1, method='sha256'))
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     login_user(new_user, remember=True)
+        #     flash('Account created!', category='success')
+        #     return redirect(url_for('views.home'))
+        else:
+            flash('Created account with success.', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("register.html")
