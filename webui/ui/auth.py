61fc27e52bae2ccd3417c1b16f13651208860bc0
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 16:14:22 +0100

    webui: auth.py: Check if email is already registered  and change hash method

--- webui/ui/auth.py
+++ webui/ui/auth.py
@@ -32,29 +32,30 @@ def register():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        # user = User.query.filter_by(email=email).first()
-        # if user:
-        #     flash('Email already exists.', category='error')
-        if len(name) < 2:
-            flash('Name must be greater than 1 character.', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', 'error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', 'error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+            flash('Passwords don\'t match.', 'error')
         elif len(password1) < 6:
-            flash('Password must be at least 6 characters.', category='error')
+            flash('Password must be at least 6 characters.', 'error')
         else:
-
             if len(User.query.all()) == 0:
                 role = "Admin"
             else:
                 role = "User"
             new_user = User(email=email,
                             password=generate_password_hash(password1,
-                                                            method='sha256'),
+                                                            method='pbkdf2:'
+                                                            'sha256:100',
+                                                            salt_length=16),
                             name=name, role=role)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Account created!', 'success')
             return redirect(url_for('views.home'))
 
     return render_template("register.html")
