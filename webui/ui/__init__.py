79cc2584ad4bcc9216f6184e6eff77dfc1088688
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 02:02:18 +0100

    webui: init.py: Add LoginManager

--- webui/ui/__init__.py
+++ webui/ui/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_toastr import Toastr
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -26,6 +27,15 @@ def create_app():
     from .models import User
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
 
     return app
 
