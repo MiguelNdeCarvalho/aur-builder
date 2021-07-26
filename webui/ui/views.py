e984c879cd4bfef587396e27990948eb3c2d6f9c
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-26 16:42:41 +0100

    webui: views.py: Add gravatar and add required login

--- webui/ui/views.py
+++ webui/ui/views.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from libgravatar import Gravatar
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
+@login_required
 def home():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user,
+                           gravatar=getGravatar(current_user, 160))
+
+
+def getGravatar(user, size):
+    g = Gravatar(user.email)
+    return g.get_image(size=size)
