2e7876e13d9605a7e61187bf6e353e35363b235b
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-29 21:58:55 +0100

    webui: views:py: Handle accounts not active and handle admin and normal users

--- webui/ui/views.py
+++ webui/ui/views.py
@@ -9,13 +9,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    if current_user.role == "Admin":
-        return render_template('index.html', user=current_user,
-                               users=User.query.all(),
-                               gravatar=getGravatar(current_user, 160))
+    if current_user.is_active():
+        if current_user.is_admin():
+            return render_template('index.html', user=current_user,
+                                    packages=2,
+                                    registeredUsers=len(User.query
+                                                        .filter_by(active=True)
+                                                        .all()),
+                                    pendingUsers=len(User.query
+                                                    .filter_by(active=False)
+                                                    .all()),
+                                    pendingRequests=5,
+                                    gravatar=getGravatar(current_user, 160))
+        else:
+            return render_template('index.html', user=current_user,
+                                    gravatar=getGravatar(current_user, 160))
     else:
-        return render_template('index.html', user=current_user,
-                               gravatar=getGravatar(current_user, 160))
+        return "oi"
 
 
 def getGravatar(user, size):
