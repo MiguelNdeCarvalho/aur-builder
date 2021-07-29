372a2b45d5855af57959a300878f67be1ddd8747
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-29 20:21:25 +0100

    webui: views.py: If user has role Admin

--- webui/ui/views.py
+++ webui/ui/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from libgravatar import Gravatar
+from .models import User
 
 views = Blueprint('views', __name__)
 
@@ -8,8 +9,13 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template('index.html', user=current_user,
-                           gravatar=getGravatar(current_user, 160))
+    if current_user.role == "Admin":
+        return render_template('index.html', user=current_user,
+                               users=User.query.all(),
+                               gravatar=getGravatar(current_user, 160))
+    else:
+        return render_template('index.html', user=current_user,
+                               gravatar=getGravatar(current_user, 160))
 
 
 def getGravatar(user, size):
