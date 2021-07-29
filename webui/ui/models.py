781cbef8120c38fa948a924381c23a4d975b4584
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-29 21:41:34 +0100

    webui: models.py: User: Add admin and active and add functions to check

--- webui/ui/models.py
+++ webui/ui/models.py
@@ -4,7 +4,14 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    name = db.Column(db.String(150))
-    role = db.Column(db.String(50))
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    admin = db.Column(db.Boolean(), nullable=False, default=False)
+    active = db.Column(db.Boolean(), nullable=False, default=False)
+
+    def is_admin(self):
+        return self.admin
+
+    def is_active(self):
+        return self.active
